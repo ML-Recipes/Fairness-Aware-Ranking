aaec11070d19c31c6ef588b6c6eee3225f9e48a4
nattiya <nattiya@gmail.com>
2021-02-26 11:21:53 +0700

    Simulated search traffic for multiple cities

--- script/elastic_app_search.sh
+++ script/elastic_app_search.sh
@@ -1,17 +1,15 @@
-curl -X GET 'localhost:3002/api/as/v1/engines/ml-recipes/logs/api' \
+curl -X GET 'localhost:3002/api/as/v1/engines/airbnb-history-boston/logs/api' \
 -H 'Content-Type: application/json' \
--H 'Authorization: Bearer private-ea3kbk4jaarxhhagneqsgm9m' \
+-H 'Authorization: Bearer private-6jj3ai4ckkq2xykcocosmv6o' \
 -d '{
   "filters": {
     "date": {
-      "from": "2021-01-15T00:00:00+00:00",
-      "to": "2021-02-16T00:00:00+00:00"
+      "from": "2021-02-20T00:00:00+00:00",
+      "to": "2021-02-26T00:00:00+00:00"
     }
   },
   "page": {
-    "total_results": 1000,
-    "size": 100
-  },
-  "query": "",
-  "sort_direction": "desc"
+    "total_results": 100,
+    "size": 20
+  }
 }'
