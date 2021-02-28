4d91732ed614a1baedc2f6901174f01adf9715da
nattiya <nattiya@gmail.com>
2021-02-28 18:17:16 +0700

    Fix the response schema of API Logs

--- spark/process_query_log.py
+++ spark/process_query_log.py
@@ -122,10 +122,10 @@ clean_request_schema = StructType([
 result_schema = StructType([
       StructField('meta', StructType([
              StructField('page', StructType([
-	             StructField('current', StringType(), True),
-	             StructField('total_pages', StringType(), True),
-	             StructField('total_results', StringType(), True),
-	             StructField('size', StringType(), True)
+	             StructField('current', IntegerType(), True),
+	             StructField('total_pages', IntegerType(), True),
+	             StructField('total_results', IntegerType(), True),
+	             StructField('size', IntegerType(), True)
              ])),
              StructField('engine', StructType([
              	StructField('name', StringType(), True)
@@ -134,109 +134,109 @@ result_schema = StructType([
              ])),
       StructField("results", ArrayType(StructType([
              StructField('_meta', StructType([
-	             StructField('score', StringType(), True)
+                   StructField('score', FloatType(), True)
+             ])),
+             StructField('index', StructType([
+                  StructField('raw', StringType(), True)
              ])),
              StructField('id', StructType([
              	StructField('raw', StringType(), True)
              ])),
              StructField('name', StructType([
-             	StructField('snippet', StringType(), True)
-             ])),
-             StructField('availability_30', StructType([
-             	StructField('snippet', StringType(), True)
+             	StructField('raw', StringType(), True)
              ])),
              StructField('listing_url', StructType([
-             	StructField('snippet', StringType(), True)
-             ])),
-             StructField('host_identity_verified', StructType([
-             	StructField('snippet', StringType(), True)
-             ])),
-             StructField('review_scores_cleanliness', StructType([
-             	StructField('snippet', StringType(), True)
-             ])),
-             StructField('availability_365', StructType([
-             	StructField('snippet', StringType(), True)
-             ])),
-             StructField('review_scores_accuracy', StructType([
-             	StructField('snippet', StringType(), True)
-             ])),
-             StructField('review_scores_location', StructType([
-             	StructField('snippet', StringType(), True)
-             ])),
-             StructField('review_scores_value', StructType([
-             	StructField('snippet', StringType(), True)
-             ])),
-             StructField('scrape_id', StructType([
-             	StructField('snippet', StringType(), True)
-             ])),
-             StructField('accommodates', StructType([
-             	StructField('snippet', StringType(), True)
-             ])),
-             StructField('last_scraped', StructType([
-             	StructField('snippet', StringType(), True)
-             ])),
-             StructField('room_type', StructType([
-             	StructField('snippet', StringType(), True)
-             ])),
-             StructField('maximum_nights', StructType([
-             	StructField('snippet', StringType(), True)
-             ])),
-             StructField('calendar_updated', StructType([
-             	StructField('snippet', StringType(), True)
-             ])),
-             StructField('crawled_date', StructType([
-             	StructField('snippet', StringType(), True)
-             ])),
-             StructField('review_scores_rating', StructType([
-             	StructField('snippet', StringType(), True)
-             ])),
-             StructField('review_scores_communication', StructType([
-             	StructField('snippet', StringType(), True)
-             ])),
-             StructField('review_scores_checkin', StructType([
-             	StructField('snippet', StringType(), True)
-             ])),
-             StructField('availability_90', StructType([
-             	StructField('snippet', StringType(), True)
-             ])),
-             StructField('minimum_nights', StructType([
-             	StructField('snippet', StringType(), True)
-             ])),
-             StructField('instant_bookable', StructType([
-             	StructField('snippet', StringType(), True)
-             ])),
-             StructField('availability_60', StructType([
-             	StructField('snippet', StringType(), True)
+             	StructField('raw', StringType(), True)
              ])),
              StructField('host_id', StructType([
-             	StructField('snippet', StringType(), True)
+                  StructField('raw', StringType(), True)
              ])),
-             StructField('first_review', StructType([
-             	StructField('snippet', StringType(), True)
+             StructField('host_identity_verified', StructType([
+             	StructField('raw', StringType(), True)
              ])),
-             StructField('index', StructType([
-             	StructField('snippet', StringType(), True)
+             StructField('scrape_id', StructType([
+                  StructField('raw', StringType(), True)
              ])),
-             StructField('last_review', StructType([
-             	StructField('snippet', StringType(), True)
+             StructField('crawled_date', StructType([
+                  StructField('raw', StringType(), True)
              ])),
-             StructField('host_is_superhost', StructType([
-             	StructField('snippet', StringType(), True)
+             StructField('last_scraped', StructType([
+                  StructField('raw', StringType(), True)
+             ])),
+             StructField('accommodates', StructType([
+                  StructField('raw', FloatType(), True)
              ])),
              StructField('guests_included', StructType([
-             	StructField('snippet', StringType(), True)
+                  StructField('raw', FloatType(), True)
              ])),
-             StructField('overall_rating', StructType([
-             	StructField('snippet', StringType(), True)
+             StructField('instant_bookable', StructType([
+                  StructField('raw', StringType(), True)
+             ])),
+             StructField('host_is_superhost', StructType([
+                  StructField('raw', StringType(), True)
              ])),
              StructField('is_business_travel_ready', StructType([
-             	StructField('snippet', StringType(), True)
+                  StructField('raw', StringType(), True)
              ])),
              StructField('cancellation_policy', StructType([
-             	StructField('snippet', StringType(), True)
+                  StructField('raw', StringType(), True)
+             ])),
+             StructField('room_type', StructType([
+                  StructField('raw', StringType(), True)
+             ])),
+             StructField('minimum_nights', StructType([
+                  StructField('raw', FloatType(), True)
+             ])),
+             StructField('maximum_nights', StructType([
+                  StructField('raw', FloatType(), True)
+             ])),
+             StructField('calendar_updated', StructType([
+                  StructField('raw', StringType(), True)
+             ])),
+             StructField('availability_30', StructType([
+                  StructField('raw', FloatType(), True)
+             ])),
+             StructField('availability_60', StructType([
+                  StructField('raw', FloatType(), True)
+             ])),
+             StructField('availability_90', StructType([
+                  StructField('raw', FloatType(), True)
+             ])),
+             StructField('availability_365', StructType([
+             	StructField('raw', FloatType(), True)
+             ])),
+             StructField('first_review', StructType([
+                  StructField('raw', StringType(), True)
+             ])),
+             StructField('last_review', StructType([
+                  StructField('raw', StringType(), True)
+             ])),
+             StructField('review_scores_accuracy', StructType([
+             	StructField('raw', FloatType(), True)
+             ])),
+             StructField('review_scores_cleanliness', StructType([
+                  StructField('raw', FloatType(), True)
+             ])),
+             StructField('review_scores_location', StructType([
+             	StructField('raw', FloatType(), True)
+             ])),
+             StructField('review_scores_value', StructType([
+             	StructField('raw', FloatType(), True)
+             ])),
+             StructField('review_scores_checkin', StructType([
+             	StructField('raw', FloatType(), True)
+             ])),
+             StructField('review_scores_communication', StructType([
+                  StructField('raw', FloatType(), True)
+             ])),
+             StructField('review_scores_rating', StructType([
+                  StructField('raw', FloatType(), True)
+             ])),
+             StructField('overall_rating', StructType([
+             	StructField('raw', FloatType(), True)
              ])),
              StructField('price', StructType([
-             	StructField('snippet', StringType(), True)
+             	StructField('raw', FloatType(), True)
              ])),
          ])), True)
   ])
@@ -262,7 +262,7 @@ search_logs.toPandas().to_csv(index_name + "-raw-search-logs.csv")
 
 rankedResults = search_logs.withColumn("request", from_json(col('request_body'), clean_request_schema)).withColumn('data', from_json(col('response_body'), result_schema)).select("timestamp", col("request.query").alias("query"), col("request.filters.crawled_date").alias("filter_crawled_date"), col("request.filters.host_is_superhost").alias("filter_host_is_superhost"), col("request.filters.instant_bookable").alias("filter_instant_bookable"), col("request.filters.room_type").alias("filter_room_type"), col("request.filters.minimum_nights").alias("filter_minimum_nights"), col("request.filters.availability_30.from").alias("filter_availability_30"), col("request.filters.accommodates.from").alias("filter_accommodates"), col("request.filters.maximum_nights").alias("filter_maximum_nights"), col("request.filters._score").alias("_score"), col("request.filters.overall_rating").alias("overall_rating"), col("data.meta.request_id").alias("search_id"), col("data.meta.page.current").alias("paginated_id"), col("data.meta.page.total_pages").alias("total_pages"), col("data.meta.page.total_results").alias("total_results"), col("data.meta.page.size").alias("result_size"), posexplode("data.results"))
 
-impressions = rankedResults.select("timestamp", "query", "filter_crawled_date", "filter_host_is_superhost", "filter_instant_bookable", "filter_room_type", "filter_minimum_nights", "filter_availability_30", "filter_accommodates", "filter_maximum_nights", "search_id", "paginated_id", "total_pages", "total_results", "result_size", (col("pos")+1).alias("position"), col("col._meta.score").alias("score"), col("col.id.raw").alias("doc_id"), col("col.index.snippet").alias("index"), col("col.name.snippet").alias("title"), col("col.availability_30.snippet").alias("availability_30"), col("col.listing_url.snippet").alias("listing_url"), col("col.review_scores_rating.snippet").alias("review_scores_rating"), col("col.review_scores_cleanliness.snippet").alias("review_scores_cleanliness"), col("col.review_scores_communication.snippet").alias("review_scores_communication"), col("col.review_scores_checkin.snippet").alias("review_scores_checkin"), col("col.availability_90.snippet").alias("availability_90"), col("col.availability_365.snippet").alias("availability_365"), col("col.review_scores_accuracy.snippet").alias("review_scores_accuracy"), col("col.review_scores_location.snippet").alias("review_scores_location"), col("col.review_scores_value.snippet").alias("review_scores_value"), col("col.availability_60.snippet").alias("availability_60"), col("col.scrape_id.snippet").alias("scrape_id"), col("col.last_scraped.snippet").alias("last_scraped"), col("col.host_id.snippet").alias("host_id"), col("col.first_review.snippet").alias("first_review"), col("col.last_review.snippet").alias("last_review"), col("col.overall_rating.snippet").alias("overall_rating"), col("col.price.snippet").alias("price"))
+impressions = rankedResults.select("timestamp", "query", "filter_crawled_date", "filter_host_is_superhost", "filter_instant_bookable", "filter_room_type", "filter_minimum_nights", "filter_availability_30", "filter_accommodates", "filter_maximum_nights", "search_id", "paginated_id", "total_pages", "total_results", "result_size", (col("pos")+1).alias("position"), col("col._meta.score").alias("score"), col("col.index.raw").alias("index"), col("col.id.raw").alias("doc_id"), col("col.name.raw").alias("title"), col("col.listing_url.raw").alias("listing_url"), col("col.host_id.raw").alias("host_id"), col("col.host_identity_verified.raw").alias("host_identity_verified"), col("col.scrape_id.raw").alias("scrape_id"), col("col.crawled_date.raw").alias("crawled_date"), col("col.last_scraped.raw").alias("last_scraped"), col("col.accommodates.raw").alias("accommodates"), col("col.guests_included.raw").alias("guests_included"), col("col.instant_bookable.raw").alias("instant_bookable"), col("col.host_is_superhost.raw").alias("host_is_superhost"), col("col.is_business_travel_ready.raw").alias("is_business_travel_ready"), col("col.cancellation_policy.raw").alias("cancellation_policy"), col("col.room_type.raw").alias("room_type"), col("col.minimum_nights.raw").alias("minimum_nights"), col("col.maximum_nights.raw").alias("maximum_nights"), col("col.calendar_updated.raw").alias("calendar_updated"), col("col.availability_30.raw").alias("availability_30"), col("col.availability_60.raw").alias("availability_60"), col("col.availability_90.raw").alias("availability_90"), col("col.availability_365.raw").alias("availability_365"), col("col.first_review.raw").alias("first_review"), col("col.last_review.raw").alias("last_review"), col("col.review_scores_accuracy.raw").alias("review_scores_accuracy"), col("col.review_scores_cleanliness.raw").alias("review_scores_cleanliness"), col("col.review_scores_location.raw").alias("review_scores_location"), col("col.review_scores_value.raw").alias("review_scores_value"), col("col.review_scores_checkin.raw").alias("review_scores_checkin"), col("col.review_scores_communication.raw").alias("review_scores_communication"), col("col.review_scores_rating.raw").alias("review_scores_rating"), col("col.overall_rating.raw").alias("overall_rating"), col("col.price.raw").alias("price"))
 
 impressions.toPandas().to_csv(index_name + '-impressions.csv')
 
